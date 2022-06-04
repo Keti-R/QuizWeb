595b199622a74e6b9006f6ff9c72c2f71eadfb31
Keti Ruadze <76898185+Keti-R@users.noreply.github.com>
2022-06-04 12:20:58 +0400

    Update index.js

--- Orders/index.js
+++ Orders/index.js
@@ -1,16 +1,16 @@
 const express = require("express");
-const userRouter = require("./routes/user");
-const postRouter = require("./routes/posts");
-const productRouter = require("./routes/product");
-const orderRouter = require("./routes/order");
+const userRouter = require("./user");
+const postRouter = require("./posts");
+const productRouter = require("./product");
+const orderRouter = require("./order");
 const PORT = 8000;
 const app = express();
 
 app.use(express.json());
 app.use("/user", userRouter);
 app.use("/post", postRouter);
-const productRouter = require("./routes/product");
-const orderRouter = require("./routes/order");
+const productRouter = require("./product");
+const orderRouter = require("./order");
 
 app.get("/", (req, res) => {
   return res.json({ status: true });
